9a935d4e6199d1a1dffccbed6f2f50103eb25775
vorotna.andriana <vorotna.andriana01@gmail.com>
2021-11-18 13:55:59 +0200

    Added router instance to index

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 
+
+use core\Router;
+
+session_start();
+
 require_once "application/lib/dev.php";
 require_once "vendor/autoload.php";
-
 define("ROOT",  __DIR__);
+
+
+$router = new Router();
+$router->run();
